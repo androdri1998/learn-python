57f38d186ff31c190d4102fa8cce964c38e2b0ad
André Rodrigues <andre.rodrigues98@outlook.com>
2021-01-09 21:53:04 -0300

    fixed ex008.py

--- exercices/ex008.py
+++ exercices/ex008.py
@@ -2,10 +2,26 @@ meters_value = input('How many meters do you want to convert? ')
 
 try:
     meters = float(meters_value)
+    kilometers = meters / 1000
+    hectometers = meters / 100
+    decameters = meters / 10
+    decimeters = meters * 10
     centimeters = meters * 100
     millimeters = meters * 1000
 
-    print("{:.2f} meters contain {:.0f} centimeters and also can contain {:.0f} millimeters".format(
-        meters, centimeters, millimeters))
+    print("{:.2f} meters equal to: ".format(
+        meters))
+    print("{:.3f}km".format(
+        kilometers))
+    print("{:.3f}hm".format(
+        hectometers))
+    print("{:.3f}dam".format(
+        decameters))
+    print("{:.0f}dm".format(
+        decimeters))
+    print("{:.0f}cm".format(
+        centimeters))
+    print("{:.0f}mm".format(
+        millimeters))
 except ValueError:
     print("It isn't a number")
